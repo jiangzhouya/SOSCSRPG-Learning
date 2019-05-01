faad03107d53d5fe7a2077bd28082d77645817c0
Zhouya JIANG <jiangzhouya@live.cn>
2019-05-01 21:04:40 +0800

    创建了ModelViews并增加GameSession类

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        internal Player _currentPlayer;
+
+        Player CurrentPlayer { get => _currentPlayer; set => _currentPlayer = value; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
