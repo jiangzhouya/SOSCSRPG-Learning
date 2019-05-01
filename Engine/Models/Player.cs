0db961d003f0b79bff8f2581c25692fc9a7c6dd2
Zhouya JIANG <jiangzhouya@live.cn>
2019-05-01 20:53:18 +0800

    制作了主页面，增加了Player类的基本属性和字段

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        private string _name;
+        private string _characterClass;
+        private int _hitPoints;
+        private int _experiencePoints;
+        private int _level;
+        private int _gold;
+        public string Name { get => _name; set => _name = value; }
+        public string CharacterClass { get => _characterClass; set => _characterClass = value; }
+        public int HitPoints { get => _hitPoints; set => _hitPoints = value; }
+        public int ExperiencePoints { get => _experiencePoints; set => _experiencePoints = value; }
+        public int Level { get => _level; set => _level = value; }
+        public int Gold { get => _gold; set => _gold = value; }
+    }
+}
