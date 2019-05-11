c024a1fc40d308127942ececbc4c7ab067dc9517
Zhouya JIANG <jiangzhouya@live.cn>
2019-05-11 13:19:26 +0800

    由于微软脑残的问题，用教程的方法无法载入图片 识别当前项目文件夹，然后找到图片的绝对路径加载Monster图片

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Monster :BaseNotificationClass
+    public class Monster : BaseNotificationClass
     {
         private int _hitPoints;
 
@@ -20,7 +20,7 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(HitPoints));
             }
         }
-        public string Name { get; set;}
+        public string Name { get; set; }
         public string ImageName { get; set; }
         public int MaximumHitPoints { get; private set; }
 
@@ -32,8 +32,16 @@ namespace Engine.Models
         public Monster(string name, string imageName, int maximumHitPoints, int hitPoints, int rewardExperiencePoints, int rewardGold)
         {
             Name = name;
-            
-            ImageName = string.Format(@"E:\C#Project\SOSCSRPG\Engine\Images\Monsters\{0}", imageName);// 不知道为什么不加载图片，所以就直接用路径来索引资源
+            // 把当前项目文件夹搞到然后再把图片的绝对路径名称搞过来，方便加载图片
+            string path = Environment.CurrentDirectory;
+            int indexNum = path.IndexOf(@"WPFUI\bin", 0, path.Length);
+            string folderPath = path.Remove(indexNum);
+            ImageName = string.Format(@"{0}Engine\Images\Monsters\{1}", folderPath, imageName);
+            //
+
+        
+
+            //ImageName = string.Format(@"E:\C#Project\SOSCSRPG\Engine\Images\Monsters\{0}", imageName);// 不知道为什么不加载图片，所以就直接用路径来索引资源
             MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
             RewardExperiencePoints = rewardExperiencePoints;
@@ -43,6 +51,6 @@ namespace Engine.Models
         }
 
 
-        
+
     }
 }
